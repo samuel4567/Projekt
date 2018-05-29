d822ffa1e1f9ffc798509f5b0699788e72e4f991
Unknown <samuel.hubersh@gmail.com>
2018-05-29 17:30:13 +0200

    Veränderung bei Main
    Die Main hat nur mehr einen nutzen um zu Testen.

--- Login/Loginsystem/src/Main.java
+++ Login/Loginsystem/src/Main.java
@@ -3,59 +3,46 @@ import java.io.IOException;
 import java.sql.Date;
 
 public class Main {
-	public static Admin a=new Admin("1");
-	public static void init() {
-		
-	}
-	public static User einlogen(boolean benutzer,String name,String passwd) throws IOException {
-		
-		
-		a.rezeptionistenhinzufugen("Klaus", "Klaus", "Klausi123");
-		CsvFileReader.readCsvFile("Login.csv");
-		if (benutzer == true) {//csv
-		/*	BufferedReader fileReader = new BufferedReader(new FileReader("Login.csv"));
-			fileReader.readLine();
-			String COMMA_DELIMITER=",";
-			String line=fileReader.readLine();
-			
-			while ((line = fileReader.readLine()) != null) {
-				String[] tokens = line.split(COMMA_DELIMITER);
-				if (tokens.length > 0) {
-					Rezeptionist rezeptionist = new Rezeptionist(Integer.parseInt(tokens[0]), tokens[1], tokens[2],
-							tokens[3],tokens[4]);
-
-				
-				}
-			}*/
-			if(name.equals("admin")) {
-				return CsvFileReader.csvvergleichenadmin("Login.csv", passwd);
-				
-			}else {
-				return CsvFileReader.csvvergleichen("Login.csv", name, passwd);
-			}
-		}
-		else if(benutzer == false) {//gast keine ‹berpr¸fung notwendig
-			return new Gast(passwd, passwd, passwd, passwd, 0, passwd);
-		}
-		
-		return null;
-	}
+	public static Admin a = new Admin();// Admin wird inizialisiert
 
 	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		System.out.println("Test start");
-		User benutzer;
-	//	do {
-			
-			
-		 benutzer= einlogen(true,"reklakla","Klausi123");
-//		}while(benutzer==null);
-		 System.out.println(benutzer+"###perfekt");
-		 Gast gast=new Gast("Kevin", "Maier", "Klausen Langreinstraﬂe 5", "*Ausweisnummer*", Gast.getGaste(), null);
-		benutzer.restaurant_buchen(Date.valueOf("2018-3-17"), Date.valueOf("2018-3-23"),gast);
-		benutzer.restaurant_buchen(Date.valueOf("2018-3-17"), Date.valueOf("2018-3-24"),gast);
-		CsvFileReader.readCsvFile("restaurant.csv");
+		System.out.println("###############Start###############");
+		Rezeptionist res = new Rezeptionist(15, "rekevkev2", "Kevin", "Kevin", "666test");
+		System.out.println("res erstellt");
+		// a.rezeptionistenhinzufugen("Kevin", "Kevin", "123");
+		// a.rezeptionistenhinzufugen("Kevin", "Kevin", "123");
+		// a.rezeptionistenhinzufugen("Simon", "Unterhofer", "123");
+		// a.rezeptionistenloschen("rekevkev1");
+		// System.out.println(benutzer+"###perfekt");
+		Gast gast = new Gast("Kevin", "Maier", "Klausen Langreinstraﬂe 5", "*Ausweisnummer*", "*Steuernummer*");
+		Gast gast1 = new Gast("Michel", "Bauer", "Klausen Langreinstraﬂe 2", "*Ausweisnummer*", "*Steuernummer*");
+		Gast gast2 = new Gast("Jonas", "Nusser", "Klausen Langreinstraﬂe 2", "*Ausweisnummer*", "*Steuernummer*");
+		Gast gast3 = new Gast("Philipp", "Leitner", "Klausen Langreinstraﬂe 2", "*Ausweisnummer*", "*Steuernummer*");
+
+		gast.leistung_buchen(Date.valueOf("2018-3-17"), Date.valueOf("2018-3-30"), gast, 2);
+		gast.leistung_buchen(Date.valueOf("2018-3-17"), Date.valueOf("2018-3-23"), gast, 4);
+		gast3.leistung_buchen(Date.valueOf("2018-3-17"), Date.valueOf("2018-3-30"), gast3, 2);
+		gast1.leistung_buchen(Date.valueOf("2018-3-17"), Date.valueOf("2018-3-24"), gast1, 3);
+		gast2.leistung_buchen(Date.valueOf("2018-3-17"), Date.valueOf("2018-3-23"), gast2, 5);
+		gast.leistung_buchen(Date.valueOf("2018-3-17"), Date.valueOf("2018-3-23"), gast, 4);
+		gast2.leistung_buchen(Date.valueOf("2018-3-17"), Date.valueOf("2018-3-23"), gast2, 2);
+		gast3.zimmer_buchen(Date.valueOf("2018-3-17"), Date.valueOf("2018-3-23"), true, 1, true, gast3);
+		// CsvFileReader.readCsvFile("restaurant.csv");
 		CsvFileReader.readLeistungCsvFile("restaurant.csv");
+		System.out.println("restaurant auslesen");
+		Gast[] gaste = CsvFileReader.getgaste();
+		System.out.println("Benutzer:");
+		System.out.println(Gast.getGaste());
+		System.out.println("Rezeptionisten:");
+		res.setVorname("Michael");
+		System.out.println(Rezeptionist.getAnzahlrez());
+		System.out.println(a.rezeptionistenbearbeiten(res));
+		System.out.println(res.getId());
+		/*
+		 * for(int i=0;i<gaste.length;i++) { gaste[i].printinfo(); }
+		 */
+		System.out.println("###############Ende################");
+		return;
 	}
 
 }
