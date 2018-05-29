152d34b25995b6b76efa17995d95e4d039cd1d31
Unknown <samuel.hubersh@gmail.com>
2018-05-29 17:17:45 +0200

    Veränderung bei User
    Die Funktionen von User wurden auf ein Minimum gekürzt und Vereinfacht.

--- Login/Loginsystem/src/User.java
+++ Login/Loginsystem/src/User.java
@@ -3,23 +3,9 @@ import java.util.Date;
 
 public abstract class User {
 	//1 Zimmer  2 Sauna 3 Schwimmbad 4 Restaurant 5 Kegelbahn
-	public int schwimmbad_buchen(Date von, Date bis,Gast a) {
-		Zusatzleistungen leistung=new Zusatzleistungen(von,bis,3,a);
-		try {
-			CsvFileWriter.csv_schreiben(leistung);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			return -1;
-		}
-		return 1;
-	}
-	//Date von,Date bis,int art,String steuernummer,String vorname,String nachname) {
-		
 
-
-	public int sauna_buchen(Date von, Date bis,Gast a) {
-		Zusatzleistungen leistung=new Zusatzleistungen(von,bis,2,a);
+	public int leistung_buchen(Date von, Date bis,Gast a,int art) {
+		Zusatzleistungen leistung=new Zusatzleistungen(von,bis,art,a);
 		try {
 			CsvFileWriter.csv_schreiben(leistung);
 		} catch (IOException e) {
@@ -41,27 +27,5 @@ public abstract class User {
 		return 1;
 	}
 
-	public int kegelbahn_buchen(Date von, Date bis,Gast a) {
-		Zusatzleistungen leistung=new Zusatzleistungen(von,bis,5,a);
-		try {
-			CsvFileWriter.csv_schreiben(leistung);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			return -1;
-		}
-		return 1;
-	}
-
-	public int restaurant_buchen(Date von, Date bis,Gast a) {
-		Zusatzleistungen leistung=new Zusatzleistungen(von,bis,4,a);
-		try {
-			CsvFileWriter.csv_schreiben(leistung);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			return -1;
-		}
-		return 1;
-	}
+	
 }
